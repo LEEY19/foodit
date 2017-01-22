fff32542fc13a03f59f4e3fa5aa15a1756b60beb
Yong En Lee <yongenlee@Yongs-MacBook-Pro.local>
2017-01-22 22:21:25 +0800

    cannot solve font issue

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,6 @@ module Foodit
     # -- all .rb files in that directory are automatically loaded.
     config.assets.enabled = true
 		config.assets.paths << Rails.root.join('/app/assets/font')
+    # config.assets.paths << Rails.root.join('/app/assets/fonts')
   end
 end
