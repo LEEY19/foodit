e4f5ed707b33703e165d9071d026c67580512798
Yong En Lee <yongenlee@Yongs-MacBook-Pro.local>
2017-02-05 18:56:19 +0800

    improved wording

--- app/mailers/lead_mailer.rb
+++ app/mailers/lead_mailer.rb
@@ -2,6 +2,6 @@ class LeadMailer < ApplicationMailer
   def welcome_email(lead)
     @lead = lead
     @url = 'www.potacto.com'
-    mail(to: @lead.email, from: 'info.potacto@gmail.com', subject: 'Thank you for expressing interest towards Potacto :)')
+    mail(to: @lead.email, from: 'info.potacto@gmail.com', subject: 'Thank you for joining the Potacto invite list :)')
   end
 end
