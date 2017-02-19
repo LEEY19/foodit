ab65d17dd46b5131a278f3a11b9cc5600dd6c421
Yong En Lee <yongenlee@Yongs-MacBook-Pro.local>
2017-02-19 21:31:27 +0800

    m

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,4 +1,5 @@
 class Lead < ApplicationRecord
   validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
-  validates :email, uniqueness: true
+  # validates :email, uniqueness: true
+  validates_uniqueness_of :email, :scope => :lead_type
 end
